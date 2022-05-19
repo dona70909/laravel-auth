89a044e66a6aaf4c04562090bb4334e0f0510c83
Donatella7 <campaniellodonatella@gmail.com>
2022-05-19 15:28:10 +0200

    Fix Route to the home inside welcome

--- routes/web.php
+++ routes/web.php
@@ -26,5 +26,4 @@ Route::middleware('auth')
     ->group( function() {
         Route::get('/', 'HomeController@index')
         ->name('home');
-        Route::resource('posts','PostController@index');
     });
