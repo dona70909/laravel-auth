e333bc8d291ef22a3c4e9147de6bdf231b594175
Donatella7 <campaniellodonatella@gmail.com>
2022-05-19 19:59:49 +0200

    Style single post

--- resources/views/partials/main/post.blade.php
+++ resources/views/partials/main/post.blade.php
@@ -1,13 +1,15 @@
 <section class="container-fluid">
-    <div class="col-12">
-        <img class="card-img-top img-fluid" src="{{$post->post_img}}" alt="Card image cap">
-        <div class="card-body">
-            <h5 class="card-title">{{$post->post_title}}</h5>
-            <h6>{{$post->created_at}}</h6>
-            <p class="card-text">
-                {{$post->post_text}}
-            </p>
-            <a href="#" class="btn btn-primary">More info</a>
+    <div class="row justify-content-center p-2">
+        <div class="col-8">
+            <img class="card-img-top img-fluid" src="{{$post->post_img}}" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">{{$post->post_title}}</h5>
+                <h6>{{$post->created_at}}</h6>
+                <p class="card-text">
+                    {{$post->post_text}}
+                </p>
+                {{-- <a href="#" class="btn btn-primary">More info</a> --}}
+            </div>
         </div>
     </div>
 </section>
