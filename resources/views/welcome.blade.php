6b5dfd8ca1aa6b8326c6f27c2d54a9c243c948e1
Donatella7 <campaniellodonatella@gmail.com>
2022-05-19 16:04:44 +0200

    login WIP

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,6 +69,16 @@
                 <div class="top-right links">
                     @auth
                         <a href="{{ route('home') }}">Home</a>
+
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                        </a>
+        
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-/* Route::get('/home', 'HomeController@index')->name('home'); */
+
 Route::middleware('auth')
     ->namespace('Admin')
     ->prefix('admin')
